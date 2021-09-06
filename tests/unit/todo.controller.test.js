6cf41f06b45cc2663e384a08a60be7a6ef7b8e6f
luisluft <luberluft@gmail.com>
2021-09-06 10:39:28 -0400

    add beforeeach test

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -2,8 +2,15 @@ const todoController = require("../../controllers/todo.controller");
 const todoModel = require("../../models/todo.model");
 const httpMocks = require("node-mocks-http");
 const newToDo = require("../mock-data/new-todo.json");
+let req, res, next;
 
 todoModel.create = jest.fn();
+
+beforeEach(() => {
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+  next = null;
+});
 
 describe("todoController.createTodo", () => {
   it("should have a createTodo method", () => {
@@ -11,11 +18,7 @@ describe("todoController.createTodo", () => {
   });
 
   it("should call todoModel.create", () => {
-    let req, res, next;
-    req = httpMocks.createRequest();
     req.body = newToDo;
-    res = httpMocks.createResponse();
-    next = null;
 
     todoController.createTodo(req, res, next);
     expect(todoModel.create).toBeCalledWith(newToDo);
