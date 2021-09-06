68962afdafea1ec1cf92f50c2fa2c95206181c74
luisluft <luberluft@gmail.com>
2021-09-06 09:55:28 -0400

    add model test

--- tests/unit/todo.controller.test.js
+++ tests/unit/todo.controller.test.js
@@ -1,7 +1,15 @@
 const todoController = require("../../controllers/todo.controller");
+const todoModel = require("../../models/todo.model");
+
+todoModel.create = jest.fn();
 
 describe("todoController.createTodo", () => {
   it("should have a createTodo method", () => {
     expect(typeof todoController.createTodo).toBe("function");
+  });
+
+  it("should call todoModel.create", () => {
+    todoController.createTodo();
+    expect(todoModel.create).toBeCalled();
   });
 });
